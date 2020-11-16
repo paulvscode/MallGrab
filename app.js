425970e2c92771b50fda3542c48bd69b20bb81d0
paulvscode <paulbdelagerie@gmail.com>
2020-11-16 10:35:39 +0100

    added player and mall

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const game = document.getElementById('game')
 
-app = {
+const app = {
     row: 0,
     cell: 0,
+
+    mall: {
+        xMall: 0,
+        yMall: 0,
+    },
+
+    player: {
+        xPlayer: 0,
+        yPlayer: 0,
+    }
+
 }
 
 const play = () => {
     app.row = document.getElementById('rowhtml').value
     app.cell = document.getElementById('rowhtml').value
-    console.log(`on est dans la function, et le nombre de cases est ${app.row}`)
     createBoard();
+}
+
+const randomNumber = (min, max) => {
+    let randNum = Math.floor(Math.random() * max - min) + min
+    return randNum
 }
 
 const createBoard = () => {
     game.innerHTML = ''
+    app.mall.xMall = app.row
+    app.mall.yMall = app.row 
+    console.log(app.mall.xMall)
+    console.log(app.mall.yMall)
     for (let i = 0; i < app.row; i++) {
         const createRow = document.createElement('div')
         createRow.className = 'row'
@@ -22,6 +41,15 @@ const createBoard = () => {
             const createCell = document.createElement('div')
             createCell.className = 'cell'
             createRow.appendChild(createCell)
+
+            if (i === (app.mall.xMall - 1) && j === (app.mall.yMall - 1)) {
+                createCell.classList.add('mall')
+            }
+
+            if (i === app.player.xPlayer && j === app.player.xPlayer) {
+                createCell.classList.add('player')
+            }
+
         }
     
         game.appendChild(createRow)
@@ -30,4 +58,17 @@ const createBoard = () => {
 
 const clean = () => {
     game.innerHTML = ''
+    const casesNumber = document.getElementById('rowhtml')
+    casesNumber.value = ''
 }
+
+// obstacles
+// if (i === app.mall.xMall && j === app.mall.yMall) {
+            //     createCell.classList.add('green')
+            // }
+// const randObstacles = (max) => {
+//     let x = randomNumber(0, max)
+//     let y = randomNumber(0, max)
+//     app.mall.xMall = x
+//     app.mall.yMall = y
+// }
